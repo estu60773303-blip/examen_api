57e8a16cd2f9194c9e53fc33dc0c8e13402757ee
Edinson Carlos Florian Gutierrez <edinsoncarlosflorian@gmail.com>
2026-06-26 17:03:16 -0500

    examen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/character_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rick and Morty App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CharacterListScreen(),
+    );
+  }
+}
